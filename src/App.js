b3d484fac760f12d83785205d56961f4512b0650
Gbemisola Odukoya <folaryz@gmail.com>
2022-08-17 23:41:55 +0100

    made changes

--- src/App.js
+++ src/App.js
@@ -2,32 +2,31 @@ import React from "react";
 import Jokes from "./Jokes.jsx";
 
 function App() {
-  return (
-    <div>
-      <Jokes
-        setup="Why don't pirates travel on mountain roads?"
-        punchline="Scurvy."
-      />
-      <Jokes
-        setup="I got my daughter a fridge for her birthday."
-        punchline="I can't wait to see her face light up when she opens it."
-      />
-
-      <Jokes
-        setup="How did the hacker escape the police?"
-        punchline="He just ransomware!"
-      />
-
-      <Jokes
-        setup="Why do bees stay in a hive in the winter?"
-        punchline="Swarm."
-      />
-      <Jokes
-        setup="What's the best thing about Switzerland?"
-        punchline="I don't know, but the flag is a big plus!"
-      />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
+{
+  /* <Jokes
+setup="Why don't pirates travel on mountain roads?"
+punchline="Scurvy."
+/>
+<Jokes
+setup="I got my daughter a fridge for her birthday."
+punchline="I can't wait to see her face light up when she opens it."
+/>
+
+<Jokes
+setup="How did the hacker escape the police?"
+punchline="He just ransomware!"
+/>
+
+<Jokes
+setup="Why do bees stay in a hive in the winter?"
+punchline="Swarm."
+/>
+<Jokes
+setup="What's the best thing about Switzerland?"
+punchline="I don't know, but the flag is a big plus!"
+/> */
+}
