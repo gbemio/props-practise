f39c88877c8d74f4fcfd9c431f08efee8895bf07
Gbemisola Odukoya <folaryz@gmail.com>
2022-08-18 13:20:14 +0100

    Added mapping component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from "react";
 import Jokes from "./Jokes.jsx";
+import JokesData from "./JokesData.jsx";
 
 function App() {
-  return <div></div>;
+  const jokeElements = JokesData.map((joke) => {
+    return <Jokes setup={joke.setup} punchline={joke.punchline} />;
+  });
+  return <div>{jokeElements}</div>;
 }
 
 export default App;
