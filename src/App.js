b4b309e5e9791b4ddefb5ba8db4030384ea821d3
Gbemisola Odukoya <folaryz@gmail.com>
2022-09-15 07:25:45 +0100

    Added ToggleShown Function for practise

--- src/App.js
+++ src/App.js
@@ -10,27 +10,3 @@ function App() {
 }
 
 export default App;
-{
-  /* <Jokes
-setup="Why don't pirates travel on mountain roads?"
-punchline="Scurvy."
-/>
-<Jokes
-setup="I got my daughter a fridge for her birthday."
-punchline="I can't wait to see her face light up when she opens it."
-/>
-
-<Jokes
-setup="How did the hacker escape the police?"
-punchline="He just ransomware!"
-/>
-
-<Jokes
-setup="Why do bees stay in a hive in the winter?"
-punchline="Swarm."
-/>
-<Jokes
-setup="What's the best thing about Switzerland?"
-punchline="I don't know, but the flag is a big plus!"
-/> */
-}
--- src/Jokes.jsx
+++ src/Jokes.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 
 function Jokes(props) {
+  const [isShown, setIsShown] = React.useState(false);
+
+  function toggleShown() {
+    setIsShown((prevShown) => !prevShown);
+  }
   return (
     <div>
       <p className="setup">Setup: {props.setup}</p>
       <p className="punchline">Punchline: {props.punchline}</p>
+      <button onClick={toggleShown}>Show Punchline</button>
     </div>
   );
 }
